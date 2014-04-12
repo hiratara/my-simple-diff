7383836f9ff240515dda8b4c7318d2c2fd275636
hiratara <hiratara@cpan.org>
2014-04-12 15:40:36 +0900

    Implement ajax form

---
+++ static/script/control.js
@@ -0,0 +1,24 @@
+$(function ($) {
+    function submitForm ($form) {
+        $.ajax({
+            url: $form.attr("action"),
+            type: $form.attr("method"),
+            data: $form.serialize(),
+            timeout: 10000,
+            success: function(result, textStatus, xhr) {
+                $("#display-result").html(result);
+            },
+            error: function(xhr, textStatus, error) {
+                alert("some error detected. status=" + textStatus + ", error=" + error);
+            }
+        });
+    }
+
+    $("#main_from").change(function () {
+        submitForm($(this));
+    });
+    $("#main_from").submit(function (event) {
+        event.preventDefault();
+        submitForm($(this));
+    });
+});
