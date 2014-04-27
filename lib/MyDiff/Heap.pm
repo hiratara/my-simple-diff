962963b3c888fa207ee9ae6111908986563ba668
hiratara <hiratara@cpan.org>
2014-04-27 14:07:17 +0900

    Implement the heap list

--- lib/MyDiff/Heap.pm
+++ lib/MyDiff/Heap.pm
@@ -7,17 +7,53 @@ sub new {
     bless [] => $class;
 }
 
+sub _swap ($$) {
+    my $temp = $_[0];
+    $_[0] = $_[1];
+    $_[1] = $temp;
+    ();
+}
+
 sub push {
     my ($self, $prior, $item) = @_;
     push @$self, {prior => $prior, item => $item};
+
+    # Ordering
+    my $idx = $#$self;
+    while ($idx >= 0) {
+        my $parent_idx = int(($idx - 1) / 2);
+        last if $self->[$idx]{prior} <= $self->[$parent_idx]{prior};
+        _swap ($self->[$idx], $self->[$parent_idx]);
+        $idx = $parent_idx;
+    }
+
     ();
 }
 
 sub pop {
     my $self = shift;
-    my $item = (sort {$b->{prior} <=> $a->{prior}} @$self)[0];
-    @$self = grep { $item != $_ } @$self;
-    $item->{item};
+    @$self or return;
+
+    my $poped = $self->[0];
+
+    if (my $last_item = pop @$self and @$self) {
+        $self->[0] = $last_item;
+
+        # Ordering again
+        my $idx = 0;
+        while ((my $child_idx = $idx * 2 + 1) < @$self) {
+            # Select the bigger one
+            $child_idx++
+                if $child_idx + 1 < @$self &&
+                   $self->[$child_idx + 1]{prior} > $self->[$child_idx]{prior};
+
+            last if $self->[$child_idx]{prior} <= $self->[$idx]{prior};
+            _swap ($self->[$child_idx], $self->[$idx]);
+            $idx = $child_idx;
+        }
+    }
+
+    $poped->{item};
 }
 
 1;
